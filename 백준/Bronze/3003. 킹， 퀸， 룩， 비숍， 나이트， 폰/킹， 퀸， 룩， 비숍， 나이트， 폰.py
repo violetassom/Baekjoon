d65b58e22671635121e0eeeea088880465bcfad5
violetassom <83385561+violetassom@users.noreply.github.com>
2022-12-22 00:07:47 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -0,0 +1,16 @@
+normal_a = 1
+normal_b = 1
+normal_c = 2
+normal_d = 2
+normal_e = 2
+normal_f = 8
+
+a,b,c,d,e,f = map(int,input().split())
+diff_a = normal_a - a
+diff_b = normal_b - b
+diff_c = normal_c - c
+diff_d = normal_d - d
+diff_e = normal_e - e
+diff_f = normal_f - f
+
+print(diff_a,diff_b,diff_c,diff_d,diff_e,diff_f)
