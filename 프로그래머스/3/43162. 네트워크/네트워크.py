853ca607be22126f0434015c703d8692a1b7c98b
violetassom <83385561+violetassom@users.noreply.github.com>
2024-06-10 16:37:33 +0900

    [level 3] Title: 네트워크, Time: 0.31 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,14 +1,28 @@
+# def dfs(computers,visited,node):
+#     visited[node]=True
+#     for idx,connected in enumerate(computers[node]):
+#         if connected and not visited[idx]:
+#             dfs(computers,visited,idx)
+
+# def solution(n, computers):
+#     answer = 0
+#     visited = [False]*n
+#     for i in range(n):
+#         if not visited[i]:
+#             dfs(computers,visited,i)
+#             answer += 1
+#     return answer
 def dfs(computers,visited,node):
     visited[node]=True
     for idx,connected in enumerate(computers[node]):
-        if connected and not visited[idx]:
+        # 방문 안했으면 방문해야함
+        if not visited[idx] and connected:
             dfs(computers,visited,idx)
-
-def solution(n, computers):
+def solution(n,computers):
     answer = 0
     visited = [False]*n
     for i in range(n):
         if not visited[i]:
             dfs(computers,visited,i)
-            answer += 1
+            answer+=1
     return answer
